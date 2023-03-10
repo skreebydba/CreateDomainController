c39ab1131ab9cd365514112bf378aceab7eed64b
skreebydba <skreebydba@users.noreply.github.com>
2023-03-10 11:09:11 -0600

    Genericized variables

--- InstallDNS.ps1
+++ InstallDNS.ps1
@@ -1,3 +1,4 @@
-﻿$dnspassword = "JuanS0t022WN";
+﻿$dnspassword = "Y0ur$tr0ngP@$$w0rd";
+ $domainname = "yourdomainname.local"
 $securepassword = $dnspassword | ConvertTo-SecureString -AsPlainText -Force;
-Install-ADDSForest -DomainName fbgdeplag.local -SafeModeAdministratorPassword $securepassword -InstallDNS -Force
+Install-ADDSForest -DomainName $domainname -SafeModeAdministratorPassword $securepassword -InstallDNS -Force
